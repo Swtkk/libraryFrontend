cfca74f821384aa7e4fdb05a14e8a3ad399d7578
marci <marcin123523@wp.pl>
2023-12-04 23:08:09 +0100

    Dodano stopke do strony glownej

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,10 @@
+export default function FooterComponent() {
+    return (
+        <div className={"bg-blue-500"}>
+            <div className={"container"}>
+                <div>Biblioteka</div>
+                <div>Powrót do strony głównej!</div>
+            </div>
+        </div>
+    );
+}
