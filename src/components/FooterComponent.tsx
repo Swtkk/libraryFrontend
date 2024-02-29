09484daec90f86a90b2b20ebbd8b2f855c6bfcc2
marci <marcin123523@wp.pl>
2024-02-29 21:12:57 +0100

    Poprawiono stopke

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,9 +1,16 @@
 export default function FooterComponent() {
     return (
-        <div className={"bg-blue-500"}>
+        <div className={"bg-blue-600 p-4 text-white w-full "}>
             <div className={"container"}>
-                <div>Biblioteka</div>
-                <div>Powrót do strony głównej!</div>
+                <div className={" justify-center gap-5"}>
+                    <div>Dotarłeś na koniec strony kliknij przycisk i wróc na górę</div>
+                    <button className={" mt-2 py-2 px-4 bg-green-500 rounded-2xl border-black border-2"}
+                            onClick={() => {
+                                window.scrollTo(0, 0)
+                            }}>Powrót
+                    </button>
+                    {/*<div>Jeśli masz pytania skontakuj się</div>*/}
+                </div>
             </div>
         </div>
     );
